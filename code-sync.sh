660a7306cb2eb76a3381c395d0285683509e290c
Neeraj Dalal <admin@nrjdalal.com>
2024-04-23 11:46:38 +0530

    husky added

--- code-sync.sh
+++ code-sync.sh
@@ -9,16 +9,21 @@ cp MAIN.md NEW_README.md
 # find the string --- lazy-dev.tsx --- in the NEW_README.md file and replace it with the contents of the file ./src/components/lazy-dev/lazy-dev.tsx
 sed -i '' -e '/--- lazy-dev.tsx ---/{r ./src/components/lazy-dev/lazy-dev.tsx' -e 'd;}' NEW_README.md
 
-# uncomment // if (process.env.NODE_ENV !== 'development') return null in the NEW_README.md file
-sed -i '' -e 's|// if (process.env.NODE_ENV !== 'development') return null|if (process.env.NODE_ENV !== 'development') return null|' NEW_README.md
-
 # delete the file README.md if it exists
 rm -f README.md
 
 # move the NEW_README.md file to README.md
 mv NEW_README.md README.md
 
-# commit the changes to the repository
-git add README.md
-git commit -m "sync README.md with code"
-git push
+# move README.md to ./src/app/(mdx)/page.mdx
+cp README.md "./src/app/(mdx)/page.mdx"
+
+# remove first 2 lines from ./src/app/(mdx)/page.mdx
+sed -i '' -e '1,2d' "./src/app/(mdx)/page.mdx"
+
+# remove lines containing raw.githubusercontent.com from ./src/app/(mdx)/page.mdx
+sed -i '' -e '/raw.githubusercontent.com/d' "./src/app/(mdx)/page.mdx"
+
+# replace "// if" with "if" in ./src/app/(mdx)/page.mdx and README.md
+sed -i '' -e 's/\/\/ if/if/g' "./src/app/(mdx)/page.mdx"
+sed -i '' -e 's/\/\/ if/if/g' README.md
