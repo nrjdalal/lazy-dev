67ea93dad71a1c2b44e7e6d561fcbcfea56cb3f4
Neeraj Dalal <admin@nrjdalal.com>
2024-04-23 11:34:11 +0530

    code-sync script added

--- code-sync.sh
+++ code-sync.sh
@@ -9,6 +9,9 @@ cp MAIN.md NEW_README.md
 # find the string --- lazy-dev.tsx --- in the NEW_README.md file and replace it with the contents of the file ./src/components/lazy-dev/lazy-dev.tsx
 sed -i '' -e '/--- lazy-dev.tsx ---/{r ./src/components/lazy-dev/lazy-dev.tsx' -e 'd;}' NEW_README.md
 
+# uncomment // if (process.env.NODE_ENV !== 'development') return null in the NEW_README.md file
+sed -i '' -e 's|// if (process.env.NODE_ENV !== 'development') return null|if (process.env.NODE_ENV !== 'development') return null|' NEW_README.md
+
 # delete the file README.md if it exists
 rm -f README.md
 
